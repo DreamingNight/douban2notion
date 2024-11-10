7239412a854eccfaae77208c14be4d66c4725f3b
DreamingNight <739215370@qq.com>
2024-11-10 16:51:55 +0800

    Update douban.py

--- scripts/douban.py
+++ scripts/douban.py
@@ -92,9 +92,12 @@ def insert_movie():
     for i in movie_status.keys():
         results.extend(fetch_subjects(douban_name, "movie", i))
 
-    print(f"results: {results}")
+    # print(f"results: {results}")
+    
     for result in results:
         movie = {}
+        print(f"result: {result}")
+        
         subject = result.get("subject")
         if(subject.get("title")=="未知电影" or subject.get("title")=="未知电视剧") and subject.get("url") in unknown_dict:
             unknown = unknown_dict.get(subject.get("url"))
