db294648625999f0ace6dfc0cf6bc9b37175481b
DreamingNight <739215370@qq.com>
2024-11-10 17:00:43 +0800

    Update douban.py

--- scripts/douban.py
+++ scripts/douban.py
@@ -96,11 +96,12 @@ def insert_movie():
     
     for result in results:
         movie = {}
-
-        if result is None:
-            continue
         
         subject = result.get("subject")
+
+        if subject is None:
+            continue
+        
         if(subject.get("title")=="未知电影" or subject.get("title")=="未知电视剧") and subject.get("url") in unknown_dict:
             unknown = unknown_dict.get(subject.get("url"))
             subject["title"] = unknown.get("title")
